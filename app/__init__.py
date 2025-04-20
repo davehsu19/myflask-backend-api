73efb192fec9f510e142ad0c9d54c048ada8b9ed
David Xu <davehsu19@gmail.com>
2025-04-19 23:57:49 -0700

    more commit for my flask backend for CORS support for created db with users struct

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from .config import Config
+from .models import db  # or however you import db
+from .models import User  # import your models before creating
 
 # app = Flask(__name__)
 # Initialize extensions
